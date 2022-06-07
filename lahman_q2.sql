95378433aa00a4a5675446c6b9d93988b9c0fd09
dkhamken <dkhamken@gmail.com>
2022-06-07 11:58:59 -0500

    lahman q2 review

---
+++ lahman_q2.sql
@@ -0,0 +1,14 @@
+-- 2. Find the name and height of the shortest player in the database. How many games did he play in? 
+-- What is the name of the team for which he played?
+
+SELECT DISTINCT p.namefirst, 
+				p.namelast, 
+				p.namegiven, 
+				p.height, 
+				a.playerid, 
+				t.name
+FROM people AS p
+		LEFT JOIN appearances AS a ON a.playerid = p.playerid
+		LEFT JOIN teams AS t ON t.teamid = a.teamid
+WHERE height IN(SELECT MIN(height)
+				FROM people)
