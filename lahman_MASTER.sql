7119c07a76d2507dbe16ddf3e4d55391fd468667
dkhamken <dkhamken@gmail.com>
2022-06-09 11:07:10 -0500

    lahman q13 and updated master

--- lahman_MASTER.sql
+++ lahman_MASTER.sql
@@ -2,6 +2,7 @@
 
 
 -- 1. What range of years for baseball games played does the provided database cover? 
+-- MIN: 1871, MAX: 2016
 SELECT 
 	MIN(yearid),
 	MAX(yearid)
@@ -9,12 +10,13 @@ FROM batting;
 
 
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
+-- A: Eddie Gaedel, played in 1 game for STL browns
 SELECT DISTINCT p.namefirst, 
 				p.namelast, 
 				p.namegiven, 
-				p.height, 
-				a.playerid, 
-				t.name
+				p.height,  
+				a.g_all AS total_games_played,
+				t.name			
 FROM people AS p 
 		LEFT JOIN appearances AS a ON a.playerid = p.playerid
 		LEFT JOIN teams AS t ON t.teamid = a.teamid
@@ -24,15 +26,15 @@ WHERE height IN(SELECT MIN(height)
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they 
 -- earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors? 
--- A: David Price, David Taylor
+-- A: David Taylor Price
 SELECT DISTINCT (sa.playerid), s.schoolname, p.namefirst, p.namelast, p.namegiven,
-	   SUM(sa.salary) OVER(PARTITION BY sa.playerid) AS total_salary
+	   SUM(sa.salary::numeric::money) OVER(PARTITION BY sa.playerid) AS total_salary
 FROM schools AS s
 	LEFT JOIN collegeplaying AS cp ON cp.schoolid = s.schoolid
 	LEFT JOIN people AS p ON p.playerid = cp.playerid
 	LEFT JOIN salaries AS sa ON sa.playerid = p.playerid
 WHERE schoolname = 'Vanderbilt University'
-ORDER BY total_salary DESC;
+ORDER BY total_salary DESC NULLS LAST;
 
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", 
@@ -56,7 +58,7 @@ GROUP BY position;
 
 
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places.
--- Do the same for home runs per game. Do you see any trends?
+-- Do the same for home runs per game. Do you see any trends? INC as the years go (steriod use increased)
 WITH decades AS
 			(SELECT yearid, so, hr, g,
 	         CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
@@ -151,7 +153,6 @@ LIMIT 1;
 
 -- 7d. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series?
 -- There are two queries for 7d, with and without an INNER JOIN, each arriving at the same result.
-
 --WITHOUT inner join
 WITH win_percentage AS (SELECT yearid, name, wswin, 
 	                   (CASE WHEN w = MAX(w) OVER(PARTITION BY yearid)
@@ -159,12 +160,12 @@ WITH win_percentage AS (SELECT yearid, name, wswin,
 			            FROM teams
 			            WHERE yearid >= 1970 AND yearid <> 1981)
 SELECT --Round(SUM(max_wins)::decimal / COUNT(wswin) * 100, 1) AS max_win_perc
-	         SUM(max_wins)
+	         SUM(max_wins) AS most_wins
 FROM win_percentage
 WHERE wswin = 'Y';
 
 --WITH inner join
-SELECT COUNT(*)
+SELECT COUNT(*) AS most_wins
 FROM (SELECT MAX (w) AS max_w, yearid
       FROM teams
 	  WHERE yearid >= 1970
@@ -184,8 +185,8 @@ WITH win_percentage AS (SELECT yearid, name, wswin,
 						AND wswin = 'Y' THEN 1 ELSE 0 END) AS max_wins
 			            FROM teams
 			            WHERE yearid >= 1970 AND yearid <> 1981)
-SELECT Round(SUM(max_wins)::decimal / COUNT(wswin) * 100, 1) AS max_win_perc
-	         --SUM(max_wins)
+SELECT CONCAT(Round(SUM(max_wins)::decimal / COUNT(wswin)*100,1),'%') AS max_win_perc,
+	         SUM(max_wins) AS most_wins
 FROM win_percentage
 WHERE wswin = 'Y';
 
@@ -194,21 +195,38 @@ WHERE wswin = 'Y';
 -- (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. 
 -- Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 --Top 5
-SELECT team, park, ROUND(AVG(attendance),2) AS avg_attendance
+SELECT team, park, attendance / games AS avg_attendance
 FROM homegames
 WHERE year = '2016' AND games >= '10'
-GROUP BY team, park
+GROUP BY team, park, avg_attendance
 ORDER BY avg_attendance DESC
 LIMIT 5;
 
+SELECT team, park_name, year, games, attendance / games as average_attendance
+FROM homegames as h
+		INNER JOIN parks as p
+		ON h.park = p.park
+WHERE year = 2016
+      and games >= 10
+ORDER BY average_attendance DESC
+LIMIT 5;
+
 -- Lowest 5
-SELECT team, park, ROUND(AVG(attendance),2) AS avg_attendance
+SELECT team, park, attendance / games AS avg_attendance
 FROM homegames
 WHERE year = '2016' AND games >= '10'
-GROUP BY team, park
+GROUP BY team, park, avg_attendance
 ORDER BY avg_attendance
 LIMIT 5;
 
+SELECT team, park_name, year, games, attendance / games as average_attendance
+FROM homegames as h
+		INNER JOIN parks as p
+		ON h.park = p.park
+WHERE year = 2016
+      and games >= 10
+ORDER BY average_attendance ASC
+LIMIT 5;
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
 -- Give their full name and the teams that they were managing when they won the award.
@@ -227,16 +245,43 @@ SELECT DISTINCT CONCAT(people.namefirst, ' ' ,people.namelast) AS manager_name,
 	  FROM award_AL INNER JOIN award_NL ON award_NL.playerid = award_AL.playerid
 	  				INNER JOIN people ON people.playerid = award_AL.playerid
 					INNER JOIN managers ON managers.playerid = award_AL.playerid AND (managers.yearid = award_AL.yearid OR managers.yearid = award_NL.yearid)
-					INNER JOIN teams on teams.teamid = managers.teamid AND (teams.yearid = award_AL.yearid OR teams.yearid = award_NL.yearid);
+					INNER JOIN teams ON teams.teamid = managers.teamid AND (teams.yearid = award_AL.yearid OR teams.yearid = award_NL.yearid);
 
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, 
 -- and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
+WITH career_high AS (SELECT b.playerid,
+					        namefirst,
+					        namelast,
+					        COUNT(*) AS seasons_played,
+					        MAX(hr) AS career_high_hr
+					 FROM batting AS b
+				     INNER JOIN people AS p
+					 ON b.playerid = p.playerid
+	 			     GROUP BY b.playerid, namefirst, namelast)	 			  
+SELECT CONCAT(namefirst,' ', namelast), career_high_hr--, yearid, seasons_played				 
+FROM batting
+INNER JOIN career_high
+ON career_high.playerid = batting.playerid
+WHERE yearid = 2016
+AND hr >= 1
+AND seasons_played >= 10
+AND hr = career_high_hr
+ORDER BY career_high_hr DESC;
 
 
 -- 11. Is there any correlation between number of wins and team salary? Use data from 2000 and later to answer this question. As you do this analysis, 
 -- keep in mind that salaries across the whole league tend to increase together, so you may want to look on a year-by-year basis.
-
+-- No correlation between number of wins and team salary, graphs on excel
+SELECT t.name AS team_name, 
+	   t.yearid AS year, 
+	   t.w AS number_of_wins, 
+	   SUM(s.salary) AS team_salary
+FROM teams AS t
+	LEFT JOIN salaries AS s ON s.teamid = t.teamid AND s.yearid = t.yearid
+WHERE t.yearid >= '2000'
+GROUP BY t.name, t.yearid, t.w
+ORDER BY t.yearid, team_salary DESC;
 
 -- 12. In this question, you will explore the connection between number of wins and attendance.
 --     <ol type="a">
@@ -248,3 +293,34 @@ SELECT DISTINCT CONCAT(people.namefirst, ' ' ,people.namelast) AS manager_name,
 -- 13. It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this 
 -- claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. 
 -- Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
+SELECT COUNT(*)
+FROM people
+WHERE throws = 'R';
+-- 14480, 80% of all pitchers
+
+SELECT COUNT(*)
+FROM people
+WHERE throws = 'L';
+-- 3654, 20% of all pitchers
+
+WITH r_pitcher AS (SELECT *
+				   FROM people
+				   WHERE throws = 'R'),
+	 cy_award AS (SELECT *
+				  FROM awardsplayers
+				  WHERE awardid ILIKE 'CY%')
+SELECT COUNT(*)
+FROM r_pitcher 
+	INNER JOIN cy_award ON cy_award.playerid = r_pitcher.playerid;
+-- 75 R handed pitchers won CY Young Award, 0.52% of all R handed pitchers
+
+WITH l_pitcher AS (SELECT *
+				   FROM people
+				   WHERE throws = 'L'),
+	 cy_award AS (SELECT *
+				  FROM awardsplayers
+				  WHERE awardid ILIKE 'CY%')
+SELECT COUNT(*)
+FROM l_pitcher 
+	INNER JOIN cy_award ON cy_award.playerid = l_pitcher.playerid;
+-- 37 right handed players won CY Young Award, 1.01% of all L handed pitchers
